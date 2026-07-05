88269c25e28cfca4448439a8c6d0231af1ca4f42
DmytroK1994 <dmytrok1994@icloud.com>
2026-07-05 11:46:21 +0300

    1

---
+++ config 4.js
@@ -0,0 +1,5 @@
+// Налаштування власника застосунку. Користувачі не можуть змінити їх через інтерфейс.
+window.SLUKHAI_CONFIG = {
+  supportUrl: 'https://send.monobank.ua/jar/55J8nGRSwb',
+  supportLabel: 'Підтримати проєкт'
+};
