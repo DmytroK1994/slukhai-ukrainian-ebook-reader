7ff2a45a58030fc2ecbde213eaab669cbe157fb4
DmytroK1994 <dmytrok1994@icloud.com>
2026-07-07 00:08:14 +0300

    11

--- sw-v2.js
+++ sw-v2.js
@@ -1,5 +1,5 @@
-const CACHE='slukhai-v51';
-const CORE=['./','./index.html','./styles.css','./updates.css','./app.js','./config.js','./build-version.json','./manifest.webmanifest','./icon.svg','./vendor/epubjs/epub.min.js'];
+const CACHE='slukhai-v52';
+const CORE=['./','./index.html','./styles.css?v=52','./updates.css?v=52','./app.js?v=52','./config.js?v=52','./build-version.json?v=52','./manifest.webmanifest?v=52','./icon.svg?v=52','./vendor/epubjs/epub.min.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>event.respondWith(fetch(event.request).then(response=>{if(event.request.method==='GET'){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html')))));
